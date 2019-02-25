7f9153e78a8de7dcc9c2dd139b1e511bda97e63f
Dmitry Astapov <dastapov@gmail.com>
2019-02-25 20:52:30 +0000

    better diffs

--- filterdiff.sh
+++ filterdiff.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 filterdiff -x '*/export/*.txt' -x '*/export/*.journal' \
            -x '*/import/*/csv/*' -x '*/import/*/journal/*.journal' \
+           -x '*/import/*/generated.rules' \
            --remove-timestamps
