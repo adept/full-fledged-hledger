450ed102e2a60eed56aae7c2717abc8a552f7e37
Dmitry Astapov <dastapov@gmail.com>
2019-02-28 23:18:16 +0000

    mortgage: ignore assertions

--- 08-mortgage/export/mortgage_interest.sh
+++ 08-mortgage/export/mortgage_interest.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 year="$1"
 dir=$(dirname $0)
 echo ";; This is an auto-generated file, do not edit"
-hledger -f "${dir}/../${year}.journal" print 'not:desc:(interest for)' -x \
-    | hledger-interest -f - --source='expenses:mortgage interest' --target=liabilities:mortgage --annual=0.02 --act liabilities:mortgage -q --today \
+hledger -f "${dir}/../${year}.journal" print 'not:desc:(interest for)' -I \
+    | hledger-interest -f - --source='expenses:mortgage interest' --target=liabilities:mortgage --annual=0.02 --act liabilities:mortgage -q --today -I \
     | hledger print -f - -p "${year}"
