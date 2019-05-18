c89b04c7f34f23c2ccae5fde0415a868823bcb33
Jiří Altman <altman.jiri@gmail.com>
2019-05-18 11:41:47 +0200

    Update docker.sh
    To avoid problems with spaces in ${pwd} path "${pwd}" used instead

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker container run --rm -it -v $(pwd):/full-fledged-hledger dastapov/full-fledged-hledger:latest 
+docker container run --rm -it -v "$(pwd)":/full-fledged-hledger dastapov/full-fledged-hledger:latest 
