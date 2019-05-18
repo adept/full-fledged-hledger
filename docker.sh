94edcf071e059f071b9fde90f4168f37f205d5ff
Jiri Altman <jiri.altman@konicaminolta.cz>
2019-05-18 12:51:32 +0200

    Update.sh - support for WSL

--- docker.sh
+++ docker.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker container run --rm -it -v "$(pwd)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
+
+# check if started in WSL
+if [[ $(uname -r) =~ Microsoft$ ]]; then
+    #  if WSL
+    docker container run --rm -it -v "$(wslpath -a -m .)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
+else
+    # otherwise
+    docker container run --rm -it -v "$(pwd)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
+fi
