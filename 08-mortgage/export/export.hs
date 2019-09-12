5201b40de458d9431a7da1f90725489b635e4d7f
Dmitry Astapov <dastapov@gmail.com>
2019-09-12 22:50:13 +0100

    Updated for hledger 1.15.2

--- 08-mortgage/export/export.hs
+++ 08-mortgage/export/export.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-13.8 runghc --package shake --package directory
+-- stack --resolver lts-14.3 runghc --package shake --package directory
 import Development.Shake
 import Development.Shake.FilePath
 import Data.List
