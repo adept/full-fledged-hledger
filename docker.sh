3c9d73368ac8fa48859cbcfb39c5ed51658a65d1
Dmitry Astapov <dastapov@gmail.com>
2019-12-04 20:56:57 +0000

    docker.sh uses current group as well

--- docker.sh
+++ docker.sh
@@ -3,8 +3,8 @@
 # check if started in WSL
 if [[ $(uname -r) =~ Microsoft$ ]]; then
     #  if WSL
-    docker container run --rm -it --user $(id --user) -v "$(wslpath -a -m .)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
+    docker container run --rm -it --user $(id --user):$(id --group) -v "$(wslpath -a -m .)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
 else
     # otherwise
-    docker container run --rm -it --user $(id --user) -v "$(pwd)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
+    docker container run --rm -it --user $(id --user):$(id --group) -v "$(pwd)":/full-fledged-hledger dastapov/full-fledged-hledger:latest
 fi
