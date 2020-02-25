208e9d50e55118d32271304a923ed8a518d6ba28
Dmitry Astapov <dastapov@gmail.com>
2020-02-25 21:02:32 +0000

    fixed remortgage section for hledger-interest 1.5.4

--- 09-remortgage/export/mortgage_interest.sh
+++ 09-remortgage/export/mortgage_interest.sh
@@ -4,16 +4,7 @@ set -e -o pipefail
 year="$1"
 dir=$(dirname $0)
 echo ";; This is an auto-generated file, do not edit"
-# 2% till 2015-05
+# 2% till 2015-05, 1.8% after 2015-05
 hledger -f "${dir}/../${year}.journal" print 'not:desc:(interest for)' -I \
-    | hledger print -f - -e 2015-05 \
-    | hledger-interest -f - --source='expenses:mortgage interest' --target=liabilities:mortgage --annual=0.02 --act liabilities:mortgage -q --today -I \
-    | hledger print -f - -p "${year}" \
-    | hledger print -f - -e 2015-05
-
-# 1.8% after 2015-05
-hledger -f "${dir}/../${year}.journal" print 'not:desc:(interest for)' -I \
-    | hledger print -f - -b 2015-05 \
-    | hledger-interest -f - --source='expenses:mortgage interest' --target=liabilities:mortgage --annual=0.018 --act liabilities:mortgage -q --today -I \
-    | hledger print -f - -p "${year}" \
-    | hledger print -f - -b 2015-05
+    | hledger-interest -f - --source='expenses:mortgage interest' --target=liabilities:mortgage --annual-schedule='[(2000-01-01,0.02),(2015-05-01,0.018)]' --act liabilities:mortgage -q --today -I \
+    | hledger print -f - -p "${year}" 
