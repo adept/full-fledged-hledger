63f61180b11849a1f925a99dc4c6dc06891c03f2
Dmitry Astapov <dastapov@gmail.com>
2024-10-10 23:50:17 +0100

    Fixes required for 1.40: commodity declarations

--- 08-mortgage/export/mortgage_interest.sh
+++ 08-mortgage/export/mortgage_interest.sh
@@ -4,6 +4,9 @@ set -e -o pipefail
 year="$1"
 dir=$(dirname $0)
 echo ";; This is an auto-generated file, do not edit"
-hledger -f "${dir}/../${year}.journal" print 'not:desc:(interest for)' -I \
+(
+    cat ../commodities.journal
+    hledger -f "${dir}/../${year}.journal" print 'not:desc:(interest for)' -I
+) \
     | hledger-interest -f - --source='expenses:mortgage interest' --target=liabilities:mortgage --annual=0.02 --act liabilities:mortgage -q --today -I \
     | hledger print -f - -p "${year}"
